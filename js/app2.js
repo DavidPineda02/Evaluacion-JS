a2934c488fc5bf36a2845695f92f409b2cefd218
DavidPineda02 <davxpa.02@gmail.com>
2025-11-24 17:55:19 -0500

    Se realizo el Ejercicio 2: Sistema basico de autenticacion con Edad y Contraseña, el usuario debe ser mayor de edad y tener la contraseña exacta

--- js/app2.js
+++ js/app2.js
@@ -2,6 +2,8 @@
 // Temas: Operadores lógicos · Condicionales · Función expresada.
 // Enunciado analítico:
 
+import { validarAcceso } from "../modules/validarAcceso.js"
+
 // Una empresa maneja un sistema básico de autenticación. El aprendiz debe programar un validador que permita el ingreso solo si el usuario cumple dos condiciones: 
 
 // 1. Es mayor de edad.
@@ -21,3 +23,19 @@
 
 // Salidas:
 // • Mensaje de acceso. 
+
+// Función principal que maneja la interacción con el usuario
+export function validacion(){
+    // Solicitar edad y contraseña al usuario
+    let edad = Number(prompt("Ingrese su edad:"));
+    let psw = prompt("Ingrese su contraseña:");
+
+    // Validar el acceso usando la función importada
+    if(validarAcceso(edad, psw)){
+        // Mostrar mensaje si el acceso es concedido
+        alert("Acceso concedido.");
+    } else {
+        // Mostrar mensaje si el acceso es denegado
+        alert("Acceso denegado.");
+    }
+}
---
+++ modules/validarAcceso.js
@@ -0,0 +1,14 @@
+
+export const validarAcceso = function(edad, psw){
+    // Variable para almacenar el estado de acceso
+    let acceso = false;
+    
+    // Verificar si la edad es mayor a 18 y la contraseña es correcta
+    if(edad > 18 && psw === "1234"){
+        // Conceder acceso si ambas condiciones se cumplen
+        return acceso = true;
+    } else {
+        // Denegar acceso si alguna condición no se cumple
+        return acceso;
+    }
+}
