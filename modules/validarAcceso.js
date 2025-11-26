0a8559eaf3b272f7db896b9e5e4db283a8adc57c
DavidPineda02 <davxpa.02@gmail.com>
2025-11-26 13:30:04 -0500

    Arreglando el Ejercicio 2, en la validacion de edad y contraseña falto agregar el signo igual para verificar que la edad sea mayor o igual a 18

--- modules/validarAcceso.js
+++ modules/validarAcceso.js
@@ -3,8 +3,8 @@ export const validarAcceso = function(edad, psw){
     // Variable para almacenar el estado de acceso
     let acceso = false;
     
-    // Verificar si la edad es mayor a 18 y la contraseña es correcta
-    if(edad > 18 && psw === "1234"){
+    // Verificar si la edad es mayor o igual a 18 y la contraseña es correcta
+    if(edad >= 18 && psw === "1234"){
         // Conceder acceso si ambas condiciones se cumplen
         return acceso = true;
     } else {
